65d637bad8576568e892759474cb0d6145a333d5
Kylez <kylezli@protonmail.com>
2021-09-12 19:18:33 +0800

    Update code for macos

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import cv2
 import pytesseract
+import os
 
-images = cv2.imread("test.jpg")
+images = cv2.imread("test.png")
 gray = cv2.cvtColor(images, cv2.COLOR_BGR2GRAY)
 # cv2.imshow("Image", images)
 # cv2.imshow("Image Gray", gray)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# For windows
+if os.name == "nt":
+    pytesseract.pytesseract.tesseract_cmd = (
+        r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    )
+
 print(pytesseract.image_to_string(images))
