d1a56b24227b539b2faea692159a13366deb6eeb
Kylez <kylezli@protonmail.com>
2021-09-12 22:49:14 +0800

    macos support now

--- passButton.py
+++ passButton.py
@@ -1,22 +1,49 @@
 import pyautogui
-import keyboard
+
 from datetime import datetime
+import datetime as dt
+import os
 
-
-screenWidth, screenHeight = pyautogui.size()
-print(screenWidth, screenHeight)
-currentMouseX, currentMouseY = pyautogui.position()
 found = False
+
 pyautogui.PAUSE = 0.01
-while keyboard.is_pressed("q") == False:
-    if (
-        pyautogui.locateOnScreen("click.png", grayscale=True, confidence=0.9) != None
-        and found == False
-    ):
-        pyautogui.moveTo("click.png")
-        found = True
+click = 0
+# For windows
+if os.name == "nt":
+    while click < 5:
+        if (
+            pyautogui.locateOnScreen("click.png", grayscale=True, confidence=0.9)
+            != None
+            and found == False
+        ):
+            pyautogui.moveTo("click.png")
+            found = True
 
-    if found == True:
-        pyautogui.click()
+        if found == True:
+            pyautogui.click()
+            print("Clicking")
+            click += 1
 
-    print(datetime.now())
+        print(datetime.now())
+# For macOS
+else:
+    while click < 5:
+        if (
+            pyautogui.locateOnScreen("macClick.png", grayscale=True, confidence=0.9)
+            != None
+            and found == False
+        ):
+            target = pyautogui.locateOnScreen(
+                "macClick.png", grayscale=True, confidence=0.9
+            )
+            targetLoc = pyautogui.center(target)
+            target_x, target_y = targetLoc
+            pyautogui.moveTo(target_x / 2, target_y / 2)
+            found = True
+
+        if found == True:
+            pyautogui.click()
+            print("Clicking")
+            click += 1
+
+        print(datetime.now())
