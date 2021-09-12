7ccbb16fb71fd8c0fd066e0ac566074f1fb24161
Kylez <kylezli@protonmail.com>
2021-09-12 20:46:43 +0800

    It shows detected word in image

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import cv2
 import pytesseract
+from pytesseract import Output
 import os
-
-images = cv2.imread("test.png")
-gray = cv2.cvtColor(images, cv2.COLOR_BGR2GRAY)
-# cv2.imshow("Image", images)
-# cv2.imshow("Image Gray", gray)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
 # For windows
 if os.name == "nt":
@@ -15,4 +9,25 @@ if os.name == "nt":
         r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     )
 
-print(pytesseract.image_to_string(images))
+images = cv2.imread("test.png")
+# gray = cv2.cvtColor(images, cv2.COLOR_BGR2GRAY)
+# cv2.imshow("Image", images)
+# cv2.imshow("Image Gray", gray)
+
+result = pytesseract.image_to_data(images, output_type=Output.DICT)
+# print(result.keys())
+
+# show box
+n_boxes = len(result["text"])
+for i in range(n_boxes):
+    if float(result["conf"][i]) > 60:
+        (x, y, w, h) = (
+            result["left"][i],
+            result["top"][i],
+            result["width"][i],
+            result["height"][i],
+        )
+        images = cv2.rectangle(images, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow("Result", images)
+cv2.waitKey(0)
